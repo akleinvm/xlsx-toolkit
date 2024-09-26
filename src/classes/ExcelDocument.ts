799f18cdea3f1ed71e4e9c9475d66959ebc6b0fd
akleinvm <akleinvm@gmail.com>
2024-09-26 11:27:07 +0800

    Added the ExcelDocument class

---
+++ src/classes/ExcelDocument.ts
@@ -0,0 +1,46 @@
+import JSZip, { file } from 'jszip';
+import ExcelStyles from './ExcelStyles';
+import ExcelSharedStrings from './ExcelSharedStrings';
+import ExcelWorksheet from './ExcelWorksheet';
+import ExcelColumnConverter from './ExcelColumnConverter';
+
+
+export default class ExcelDocument {
+    private files = new Map<string, string>;
+    private sharedStrings = new ExcelSharedStrings();
+    private styles = new ExcelStyles();
+
+    public async fromArrayBuffer (arrayBuffer: ArrayBuffer) {
+        const zip = new JSZip();
+        await zip.loadAsync(arrayBuffer);
+
+        const zipFolder = zip.folder("");
+        if(zipFolder) {
+            for(const [relativePath, file] of Object.entries(zipFolder.files)) {
+                const content = await file.async('binarystring');
+                this.files.set(relativePath, content);
+            }
+        }
+
+        console.log('Parsing sharedStrings.xml...');
+        const xmlSharedStrings = this.files.get('xl/sharedStrings.xml') ?? '';
+        if(xmlSharedStrings === '') throw new Error('sharedStrings is null or undefined');
+        this.sharedStrings.fromXML(xmlSharedStrings);
+
+        console.log('Parsing styles.xml...');
+        const xmlStyles = this.files.get("xl/styles.xml") ?? '';
+        if(xmlStyles === '') throw new Error('styles is null or undefined');
+        this.styles.fromXML(xmlStyles);
+    }
+
+    public getRange (sheetNo: number, startCell: string, endCell: string) {
+        //how can I get the range??? 1. get the worksheet how??
+        console.log(`Parsing sheet${sheetNo}.xml...`);
+        const xmlWorksheet = this.files.get(`xl/worksheets/sheet${sheetNo}.xml`) ?? "";
+        if(xmlWorksheet === '') throw new Error('worksheet is null or undefined');
+        const worksheet = new ExcelWorksheet();
+        worksheet.fromXML(xmlWorksheet);
+
+    }
+    
+}
