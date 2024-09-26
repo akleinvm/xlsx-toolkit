d25bcdd74e073b8968b0cb7d7885f08872d8eb5e
akleinvm <akleinvm@gmail.com>
2024-09-26 10:57:55 +0800

    Convert ExcelSharedString class to static

--- src/classes/ExcelSharedStrings.ts
+++ src/classes/ExcelSharedStrings.ts
@@ -1,37 +1,37 @@
-export class ExcelSharedStrings {
-    private xmlDocument!: XMLDocument;
-    private sstElement!: Element;
-    private namespace!: string;
-    private stringsMap!: Map<string, number>;
+export default class ExcelSharedStrings {
+    private static xmlDocument: XMLDocument;
+    private static sstElement: Element;
+    private static namespace: string;
+    private static stringsMap: Map<string, number>;
 
     public fromXML(xmlString: string) {
-        this.xmlDocument = new DOMParser().parseFromString(xmlString, 'text/xml');
-        this.sstElement = this.xmlDocument.getElementsByTagName('sst')[0];
-        this.namespace = this.sstElement.getAttribute('xmlns') ?? '';
-        this.stringsMap = new Map();
+        ExcelSharedStrings.xmlDocument = new DOMParser().parseFromString(xmlString, 'text/xml');
+        ExcelSharedStrings.sstElement = ExcelSharedStrings.xmlDocument.getElementsByTagName('sst')[0];
+        ExcelSharedStrings.namespace = ExcelSharedStrings.sstElement.getAttribute('xmlns') ?? '';
+        ExcelSharedStrings.stringsMap = new Map();
         
-        const siElements = this.sstElement.getElementsByTagName('si');
+        const siElements = ExcelSharedStrings.sstElement.getElementsByTagName('si');
         for(let i = 0; i < siElements.length; i++) {
             const tElement = siElements[i].getElementsByTagName('t')[0];
             if(tElement && tElement.textContent) {
-                this.stringsMap.set(tElement.textContent, i)
+                ExcelSharedStrings.stringsMap.set(tElement.textContent, i)
             }
         }
     }
 
     public toString(): string {
-        this.sstElement.setAttribute('uniqueCount', this.stringsMap.size.toString());
+        ExcelSharedStrings.sstElement.setAttribute('uniqueCount', ExcelSharedStrings.stringsMap.size.toString());
 
         const xmlSerializer = new XMLSerializer();
-        return xmlSerializer.serializeToString(this.xmlDocument);
+        return xmlSerializer.serializeToString(ExcelSharedStrings.xmlDocument);
     }
 
-    public getStringIndex(string: string): number {
-        let stringIndex = this.stringsMap.get(string);
+    public static getStringIndex(string: string): number {
+        let stringIndex = ExcelSharedStrings.stringsMap.get(string);
 
         if(!stringIndex) {
-            stringIndex = this.stringsMap.size;
-            this.stringsMap.set(string, this.stringsMap.size);
+            stringIndex = ExcelSharedStrings.stringsMap.size;
+            ExcelSharedStrings.stringsMap.set(string, ExcelSharedStrings.stringsMap.size);
             const siElement = this.xmlDocument.createElementNS(this.namespace, 'si',);
             const tElement = this.xmlDocument.createElementNS(this.namespace, 't');
             tElement.textContent = string;
