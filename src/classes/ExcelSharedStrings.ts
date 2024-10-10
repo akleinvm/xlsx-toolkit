be1609afa99ea9ed69981f61d67f5df6faca7dfc
akleinvm <akleinvm@gmail.com>
2024-10-10 14:44:26 +0800

    Use array instead of maps for stability

--- src/classes/ExcelSharedStrings.ts
+++ src/classes/ExcelSharedStrings.ts
@@ -2,40 +2,36 @@ export default class ExcelSharedStrings {
     private xmlDocument!: XMLDocument;
     private sstElement!: Element;
     private namespace!: string;
-    public stringsMap!: Map<string, number>;
-    public numbersMap!: Map<number, string>;
+    public sharedStringArray!: Array<string>;
 
     public fromXML(xmlString: string) {
         this.xmlDocument = new DOMParser().parseFromString(xmlString, 'text/xml');
         this.sstElement = this.xmlDocument.getElementsByTagName('sst')[0];
         this.namespace = this.sstElement.getAttribute('xmlns') ?? '';
-        this.stringsMap = new Map();
-        this.numbersMap = new Map();
+        this.sharedStringArray = [];
         
         const siElements = this.sstElement.getElementsByTagName('si');
         for(let i = 0; i < siElements.length; i++) {
             const tElement = siElements[i].getElementsByTagName('t')[0];
             if(tElement && tElement.textContent) {
-                this.stringsMap.set(tElement.textContent, i);
-                this.numbersMap.set(i, tElement.textContent);
+                this.sharedStringArray[i] = tElement.textContent;
             }
         }
     }
 
     public toString(): string {
-        this.sstElement.setAttribute('uniqueCount', this.stringsMap.size.toString());
+        this.sstElement.setAttribute('uniqueCount', this.sharedStringArray.length.toString());
 
         return new XMLSerializer().serializeToString(this.xmlDocument);
     }
 
     public getStringIndex(string: string): number {
-        let stringIndex = this.stringsMap.get(string);
+        let stringIndex = this.sharedStringArray.findIndex((item) => item === string);
 
-        if(!stringIndex) {
-            stringIndex = this.stringsMap.size;
-            this.stringsMap.set(string, this.stringsMap.size);
-            this.numbersMap.set(this.stringsMap.size, string);
-            const siElement = this.xmlDocument.createElementNS(this.namespace, 'si',);
+        if(stringIndex === -1) {
+            stringIndex = this.sharedStringArray.length;
+            this.sharedStringArray[stringIndex] = string;
+            const siElement = this.xmlDocument.createElementNS(this.namespace, 'si');
             const tElement = this.xmlDocument.createElementNS(this.namespace, 't');
             tElement.textContent = string;
             siElement.appendChild(tElement);
@@ -45,6 +41,10 @@ export default class ExcelSharedStrings {
     }
 
     public getIndexString (index: number): string {
-        return this.numbersMap.get(index) ?? '';
+        return this.sharedStringArray[index] ?? '';
+    }
+
+    public getStringValues (): string {
+        return JSON.stringify(this.sharedStringArray);
     }
 }
